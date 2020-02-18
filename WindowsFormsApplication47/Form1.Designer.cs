730e71af2554c292ed06906f1912d6a16fdafa6d
zehra <zehra_kumas@hotmail.com>
2020-02-18 12:07:40 +0300

    proje atiyorum

---
+++ WindowsFormsApplication47/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApplication47
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(227, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Resim Yükleme";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 225);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(227, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Gri image Histogramı oluşturma ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(659, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "GaussianBlur filtreleme";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pictureBox3.Location = new System.Drawing.Point(440, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pictureBox4.Location = new System.Drawing.Point(440, 225);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(659, 287);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 48);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Canny kenar algoritması";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 437);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication47/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+
+namespace WindowsFormsApplication47
+{
+    public partial class Form1 : Form
+    {
+        Bitmap newBitmap;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //open file kullanarak bilgisayardan resim seçiyoruz ve pictureBox'a resmi ekliyoruz.
+            openFileDialog1.Filter = "resim dosyaları |" + "*.bmp;*.jpg;*.gif;*.wmf;*.tif;*.png";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                newBitmap = new Bitmap(openFileDialog1.FileName);
+            }
+
+
+        }
+
+        //kullanici buton2'ye tikladiginda resim griye döner.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            Bitmap gri = griyap(img);
+            pictureBox2.Image = gri;
+
+        }
+        //resmi gri yapan fonksiyon
+        public Bitmap griyap(Bitmap newBitmap)
+        {
+            //resmi piksel piksel dolasmak icin ic ice iki for kullandik.
+            for (int i = 0; i < newBitmap.Height - 1; i++)
+            {
+                for (int j = 0; j < newBitmap.Width - 1; j++)
+                {
+                    //resmi griye ceviren algoritma
+                    int deger = (newBitmap.GetPixel(j, i).R + newBitmap.GetPixel(j, i).G + newBitmap.GetPixel(j, i).B) / 3;
+                    Color renk;
+                    renk = Color.FromArgb(deger, deger, deger);
+                    newBitmap.SetPixel(j, i, renk);
+
+                }
+            }
+            return newBitmap;
+
+        }
+        //kullanici buton 3'e bsatiginda resme GaussianBlur filtresi uygulanmis olarak görür
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //gaussianblur algoritması
+            //resmi dolaşmak için iç içe for kullanıyoruz
+            for (int x = 1; x < newBitmap.Width; x++)
+            {
+                for (int y = 1; y < newBitmap.Height; y++)
+                {
+                    try
+                    {
+                        Color prevX = newBitmap.GetPixel(x - 1, y);
+                        Color nextX = newBitmap.GetPixel(x + 1, y);
+                        Color prevY = newBitmap.GetPixel(x, y - 1);
+                        Color nextY = newBitmap.GetPixel(x, y + 1);
+
+                        int avgR = (int)((prevX.R + nextX.R + prevY.R + nextY.R) / 4);
+                        int avgG = (int)((prevX.G + nextX.G + prevY.G + nextY.G) / 4);
+                        int avgB = (int)((prevX.B + nextX.B + prevY.B + nextY.B) / 4);
+
+                        newBitmap.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                        pictureBox3.Image = newBitmap;
+
+                    }
+                    catch (Exception) { }
+
+                }
+            }
+
+        }
+        //kullanıcı buton4'e tıklarsa canny algoritması uygulanır.
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            Bitmap GirisResmi, CikisResmi;
+            GirisResmi = new Bitmap(pictureBox1.Image);
+            int ResimGenisligi = GirisResmi.Width;
+            int ResimYuksekligi = GirisResmi.Height;
+            CikisResmi = new Bitmap(ResimGenisligi, ResimYuksekligi);
+            int SablonBoyutu = 3;
+            int ElemanSayisi = SablonBoyutu * SablonBoyutu;
+            int x, y;
+            //resim pixel pixel taranıyor.
+            for (x = (SablonBoyutu - 1) / 2; x < ResimGenisligi - (SablonBoyutu - 1) / 2; x++)
+            //Resmi taramaya şablonun yarısı kadar dış kenarlardan içeride başlayacak ve bitirecek.
+            {
+                for (y = (SablonBoyutu - 1) / 2; y < ResimYuksekligi - (SablonBoyutu - 1) / 2; y++)
+                {
+                    Color Renk;
+                    int P1,P2,P3,P4,P5,P6,P7,P8,P9;
+                    Renk = GirisResmi.GetPixel(x - 1, y - 1);
+                    P1 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x, y - 1);
+                    P2 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x + 1, y - 1);
+                    P3 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x - 1, y);
+                    P4 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x, y);
+                    P5 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x + 1, y);
+                    P6 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x - 1, y + 1);
+                    P7 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x, y + 1);
+                    P8 = Renk.R;
+                    Renk = GirisResmi.GetPixel(x + 1, y + 1);
+                    P9 = Renk.R;
+                    //Hesaplamayı yapan Sobel Temsili matrisi ve formülü.
+                    int RenkDegeri = Math.Abs((P1 + 2 * P2 + P3) - (P7 + 2 * P8 + P9)) + Math.Abs((P3 + 2 *
+                   P6 + P9) - (P1 + 2 * P4 + P7));
+                    //Renkler sınırların dışına çıktıysa, sınır değer alınacak.
+                    if (RenkDegeri > 255) RenkDegeri = 255;
+                    CikisResmi.SetPixel(x, y, Color.FromArgb(RenkDegeri, RenkDegeri, RenkDegeri));
+                }
+            }
+            //işlenen resim picturbox4'te görüntüleniyor.
+            pictureBox4.Image = CikisResmi;
+
+
+        }
+    }
+}
